62b6cdfc0c351e208c42cdcc3ce0dab2a725d107
Jim C K Flaten <jckf@jckf.no>
2014-09-11 20:51:02 +0200

    Add utility method for Proxy, and use MethodCache for lookup.

--- src/main/java/it/flaten/reflx/reflection/Interceptor.java
+++ src/main/java/it/flaten/reflx/reflection/Interceptor.java
@@ -4,7 +4,6 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Interceptor implements InvocationHandler {
@@ -20,45 +19,28 @@ public class Interceptor implements InvocationHandler {
     private final Object custom;
     private final Map<String, String> mapping;
 
-    private final Map<String, Method> methodCache;
-
     public Interceptor(Object original, Object custom, Map<String, String> mapping) {
         this.original = original;
         this.custom = custom;
         this.mapping = mapping;
-
-        this.methodCache = new HashMap<String, Method>();
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) {
-        // Create an array and a string of argument types.
-        Class[] argClasses = new Class[args.length];
-        String argClassesString = "";
-        for (int i = 0; i < args.length; i++) {
-            argClasses[i] = args[i].getClass();
-            argClassesString += argClasses[i].getSimpleName() + ",";
-        }
+        // Get a string representation of this invokation.
+        String key = MethodCache.getKey(this.original, method.getName(), args);
 
-        // A unique identifier for this method.
-        String cacheKey = this.original.getClass().getName() + "." + method.getName() + "(" + argClassesString.substring(0, argClassesString.length() - 1) + ")";
-
-        System.out.println("Intercepting " + cacheKey);
-
-        // Look for a custom method to intercept this invokation.
+        // Look for a custom handler.
         boolean isCustom = false;
-        if (this.methodCache.containsKey(cacheKey)) {
-            // Found a method in the cache.
-            method = this.methodCache.get(cacheKey);
-            isCustom = true;
-        } else if (this.mapping.containsKey(cacheKey)) {
-            // Look for a compatible method.
-            Method customMethod = ReflectionUtils.getCompatibleMethod(this.custom.getClass(), this.mapping.get(cacheKey), argClasses);
+        if (this.mapping.containsKey(key)) {
+            Method cached = MethodCache.getMethod(
+                this.custom,
+                this.mapping.get(key),
+                args
+            );
 
-            // Maybe the mappings are incorrect. Make sure we have a something.
-            if (customMethod != null) {
-                method = customMethod;
-                this.methodCache.put(cacheKey, customMethod);
+            if (cached != null) {
+                method = cached;
                 isCustom = true;
             }
         }
