a6e231bfd2aa8721d8c1ded939827d0ec998307b
Jim C K Flaten <jckf@jckf.no>
2014-09-12 12:15:27 +0200

    Implement plugin loading and test plugin class (requires external jar to load).

---
+++ src/main/java/it/flaten/reflx/plugin/ReflxPluginDescriptor.java
@@ -0,0 +1,29 @@
+package it.flaten.reflx.plugin;
+
+import it.flaten.reflx.api.PluginDescriptor;
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.Map;
+
+public class ReflxPluginDescriptor implements PluginDescriptor {
+    private final Map<String, String> data;
+
+    public ReflxPluginDescriptor(PluginContainer container) {
+        this.data = (Map<String, String>) new Yaml().load(container.getResourceAsStream("plugin.yml"));
+    }
+
+    @Override
+    public String getName() {
+        return this.data.get("name");
+    }
+
+    @Override
+    public String getVersion() {
+        return this.data.get("version");
+    }
+
+    @Override
+    public String getMain() {
+        return this.data.get("main");
+    }
+}
