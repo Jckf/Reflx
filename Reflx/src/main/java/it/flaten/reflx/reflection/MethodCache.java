36499fc14335ea52b903c503468b9b3f5f29a42d
Jim C K Flaten <jckf@jckf.no>
2014-09-15 09:21:41 +0200

    Implement method lookup from class name.

--- Reflx/src/main/java/it/flaten/reflx/reflection/MethodCache.java
+++ Reflx/src/main/java/it/flaten/reflx/reflection/MethodCache.java
@@ -8,24 +8,32 @@ public class MethodCache {
     private static final Map<String, Method> cache = new HashMap<>();
 
     public static String getKey(Object object, String methodName, Object... args) {
+        return getKey(object.getClass(), methodName, args);
+    }
+
+    public static String getKey(Class clazz, String methodName, Object... args) {
         String argClassesString = "";
         for (int i = 0; i < args.length; i++)
             argClassesString += args[i].getClass().getSimpleName() + ", ";
 
-        return object.getClass().getName() + "." + methodName + "(" + (argClassesString.length() > 0 ? argClassesString.substring(0, argClassesString.length() - 2) : "") + ")";
+        return clazz.getName() + "." + methodName + "(" + (argClassesString.length() > 0 ? argClassesString.substring(0, argClassesString.length() - 2) : "") + ")";
     }
 
     public static Method getMethod(Object object, String methodName, Object... args) {
+        return getMethod(object.getClass(), methodName, args);
+    }
+
+    public static Method getMethod(Class clazz, String methodName, Object... args) {
         Class[] argClasses = new Class[args.length];
         for (int i = 0; i < args.length; i++)
             argClasses[i] = args[i].getClass();
 
-        String key = getKey(object, methodName, args);
+        String key = getKey(clazz, methodName, args);
 
         if (cache.containsKey(key))
             return cache.get(key);
 
-        Method method = ReflectionUtils.getCompatibleMethod(object.getClass(), methodName, argClasses);
+        Method method = ReflectionUtils.getCompatibleMethod(clazz, methodName, argClasses);
 
         if (method == null)
             return null;
