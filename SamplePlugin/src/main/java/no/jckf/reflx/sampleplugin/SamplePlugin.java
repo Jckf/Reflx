88aa3fe99674db2c9482f25ccd8d9afc04aa0de0
Jim C K Flaten <jckf@jckf.no>
2014-09-19 01:29:51 +0200

    Update sample plugin to demo event system.

--- SamplePlugin/src/main/java/no/jckf/reflx/sampleplugin/SamplePlugin.java
+++ SamplePlugin/src/main/java/no/jckf/reflx/sampleplugin/SamplePlugin.java
@@ -2,12 +2,17 @@ package no.jckf.reflx.sampleplugin;
 
 import it.flaten.reflxapi.command.CommandExecutor;
 import it.flaten.reflxapi.command.CommandSender;
+import it.flaten.reflxapi.event.EventHandler;
+import it.flaten.reflxapi.event.EventListener;
+import it.flaten.reflxapi.event.player.PlayerLoginEvent;
 import it.flaten.reflxapi.plugin.Plugin;
 
-public class SamplePlugin extends Plugin implements CommandExecutor {
+public class SamplePlugin extends Plugin implements CommandExecutor, EventListener {
     @Override
     public void onEnable() {
         this.getServer().getCommandHandler().registerCommand("test", this);
+
+        this.getServer().getEventManager().registerEvents(this, this);
 
         System.out.println(this.getName() + " loaded :)");
     }
@@ -15,5 +20,10 @@ public class SamplePlugin extends Plugin implements CommandExecutor {
     @Override
     public void executeCommand(CommandSender sender, String command, String[] args) {
         System.out.println(sender.getName() + " ran test command :)");
+    }
+
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        System.out.println(event.getPlayer().getName() + " logged in :)");
     }
 }
